8bbdce5ecd56b46cf219077b918398e00a87051e
CHILLA01 <tchilaiagiorgi@gmail.com>
2026-01-27 18:24:03 +0400

    django

--- myproject/mziuri_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MziuriAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mziuri_app'
--- myproject/mziuri_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def my_name(request):
-    return HttpResponse("GIORGI TCHILAIA")
-
