2d0b38b45d3b71b9b491d33f466806b2cf3c24de
CHILLA01 <tchilaiagiorgi@gmail.com>
2026-02-17 18:16:54 +0400

    django

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,8 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from shop.views import product_list
+from ninja import NinjaAPI
+
+api = NinjaAPI()
+
+@api.get("/add")
+def add(request, a: int, b: int):
+    return {"result": a + b}
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', product_list),
+    path("api/", api.urls),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import ProductModel
 
 def product_list(request):
-    products = ProductModel.objects.all() # ბაზიდან ყველა პროდუქტის წამოღება
+    products = ProductModel.objects.all()
     return render(request, 'products.html', {'products': products})
